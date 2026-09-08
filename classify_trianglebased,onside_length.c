02cee8b2e4b523ea9f45197d76450e666c0d8750
Diksha <dahiyadiksha2739@gmail.com>
2026-09-08 15:51:13 +0530

    Day 10

---
+++ classify_trianglebased,onside_length.c
@@ -0,0 +1,41 @@
+<<<<<<< HEAD
+#include <stdio.h>
+int main(void)
+{
+	double a, b, c;
+	printf("Enter the three side lengths: ");
+	if (scanf("%lf %lf %lf", &a, &b, &c) != 3 ||
+		a <= 0 || b <= 0 || c <= 0 ||
+		a + b <= c || a + c <= b || b + c <= a) {
+		printf("Invalid triangle\n");
+		return 1;
+	}
+	if (a == b && b == c)
+		printf("Equilateral triangle\n");
+	else if (a == b || a == c || b == c)
+		printf("Isosceles triangle\n");
+	else
+		printf("Scalene triangle\n");
+	return 0;
+}
+=======
+#include <stdio.h>
+int main(void)
+{
+	double a, b, c;
+	printf("Enter the three side lengths: ");
+	if (scanf("%lf %lf %lf", &a, &b, &c) != 3 ||
+		a <= 0 || b <= 0 || c <= 0 ||
+		a + b <= c || a + c <= b || b + c <= a) {
+		printf("Invalid triangle\n");
+		return 1;
+	}
+	if (a == b && b == c)
+		printf("Equilateral triangle\n");
+	else if (a == b || a == c || b == c)
+		printf("Isosceles triangle\n");
+	else
+		printf("Scalene triangle\n");
+	return 0;
+}
+>>>>>>> 899ea47 (Add Day 16 Question 1)
