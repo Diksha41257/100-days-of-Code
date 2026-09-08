db1c2afc323947751c1d522d749bb25e1f1c4c19
Diksha <dahiyadiksha2739@gmail.com>
2026-09-08 15:53:49 +0530

    Day 11

---
+++ profit_loss_percentage.c
@@ -0,0 +1,41 @@
+<<<<<<< HEAD
+ #include <stdio.h>
+int main(void)
+{
+	float cost_price, selling_price, percentage;
+	printf("Enter cost price: ");
+	scanf("%f", &cost_price);
+	printf("Enter selling price: ");
+	scanf("%f", &selling_price);
+	if (selling_price > cost_price) {
+		percentage = ((selling_price - cost_price) / cost_price) * 100;
+		printf("Profit percentage = %.2f%%\n", percentage);
+	} else if (cost_price > selling_price) {
+		percentage = ((cost_price - selling_price) / cost_price) * 100;
+		printf("Loss percentage = %.2f%%\n", percentage);
+	} else {
+		printf("No profit, no loss.\n");
+	}
+	return 0;
+}
+=======
+ #include <stdio.h>
+int main(void)
+{
+	float cost_price, selling_price, percentage;
+	printf("Enter cost price: ");
+	scanf("%f", &cost_price);
+	printf("Enter selling price: ");
+	scanf("%f", &selling_price);
+	if (selling_price > cost_price) {
+		percentage = ((selling_price - cost_price) / cost_price) * 100;
+		printf("Profit percentage = %.2f%%\n", percentage);
+	} else if (cost_price > selling_price) {
+		percentage = ((cost_price - selling_price) / cost_price) * 100;
+		printf("Loss percentage = %.2f%%\n", percentage);
+	} else {
+		printf("No profit, no loss.\n");
+	}
+	return 0;
+}
+>>>>>>> 899ea47 (Add Day 16 Question 1)
